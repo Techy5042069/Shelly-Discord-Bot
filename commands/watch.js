32d3a84d8e2e3017708bfedf9d6d6a0ec69f23d2
techy5042069 <nepalidroid12345@gmail.com>
2021-07-15 17:26:36 +0545

    done'

--- commands/watch.js
+++ commands/watch.js
@@ -6,29 +6,32 @@ module.exports = {
         return getfunc(msg, args, obj);
     }
 };
-let mode = ['youtube', 'poker', 'chess', 'betrayal', 'fishing']
+let mod = ['youtube', 'poker', 'chess', 'betrayal', 'fishing']
 
 function getfunc(msg, args, obj) {
     togetherType = '';
     prefix = obj.prefix;
     msgEmbed = obj.msgEmbed;
-    msgEmbed.setColor('#0099ff').setTitle('**2gether command**').setDescription(`**please use the following syntax:**: ${prefix}2gether [${mode.join(',')}]`)
+    msgEmbed.setColor('#0099ff').setTitle('**2gether command**').setDescription(`**please use the following syntax:**: ${prefix}2gether [${mod.join(',')}]`)
     if (msg.member.voice.channel) {
-        console.log(prefix, msgEmbed)
+        // console.log(prefix, msgEmbed, args[0])
         if (args[0]) {
             // togetherType = mode[0];
-            for (let i = 0; i < mode.length - 1; i++) {
-                if (args[0] == mode[i]) {
-                    togetherType = mode[i]
+            for (let i = 0; i < mod.length - 1; i++) {
+                if (args[0] == mod[i]) {
+                    togetherType = mod[i]
+                    obj.client.discordTogether.createTogetherCode(msg.member.voice.channelID, mod[i]).then(async invite => {
+                        return msg.channel.send(`${invite.code}`)
+                    })
+                    break;
                 }
             }
-            obj.client.discordTogether.createTogetherCode(msg.member.voice.channelID, togetherType).then(async invite => {
-                return msg.channel.send(`${invite.code}`)
-            })
-            console.log('laskdfj')
-        } else if (togetherType = '') {
+
+            // console.log('laskdfj')
+        }
+        if (!args[0]) {
             msg.reply(msgEmbed)
-            console.log('ye?')
+            // console.log('ye?')
         }
     } else {
         msg.reply('join some VC shit!')
