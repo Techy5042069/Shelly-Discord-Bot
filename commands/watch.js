38697da887a17de2e8b733d32727bd4cd0be84a1
techy5042069 <nepalidroid12345@gmail.com>
2021-07-15 16:28:19 +0545

    watch.js fix

--- commands/watch.js
+++ commands/watch.js
@@ -6,14 +6,26 @@ module.exports = {
         return getfunc(msg, args, obj);
     }
 };
+let mode = ['youtube', 'poker', 'chess', 'betrayal', 'fishing']
 
 function getfunc(msg, args, obj) {
     if (msg.member.voice.channel) {
-        obj.client.discordTogether.createTogetherCode(msg.member.voice.channelID, 'youtube').then(async invite => {
-            return msg.channel.send(`${invite.code}`);
-        });
-    }else{
+        if (!args[0]) {
+            togetherType = mode[0];
+        } else {
+            for (let i = 0; i < mode.length - 1; i++) {
+                if (args[0] == mode[i]) {
+                    togetherType = mode[i]
+                    break;
+                }
+            }
+        }
+    } else {
         msg.reply('join some VC shit!')
+        return;
     }
+    obj.client.discordTogether.createTogetherCode(msg.member.voice.channelID, togetherType).then(async invite => {
+        return msg.channel.send(`${invite.code}`);
+    })
     console.log('runnig')
 }
